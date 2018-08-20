518db7052ab784fa648c1cb72b0c29275e591b38
melaaman <erika.laamanen@gmail.com>
2018-08-20 15:46:37 +0300

    Erikan ensimmäiset lisäykset

--- kimppakyyti-front/src/index.js
+++ kimppakyyti-front/src/index.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import "bootstrap/dist/css/bootstrap.min.css";
+// import authentication from "react-azure-adb2c";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
