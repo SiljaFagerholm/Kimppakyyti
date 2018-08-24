821e40ec261d901bc5c9b0ebbf8e8d127a4fdfb3
JukkaPeltola <jukka.peltola@mbnet.fi>
2018-08-24 10:28:58 +0300

    Kutsu googlelle ja arvojen palautus CosmosDB

--- kimppakyyti-api/KimppakyytiApi/Controllers/RideController.cs
+++ kimppakyyti-api/KimppakyytiApi/Controllers/RideController.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using KimppakyytiApi.Models;
+using KimppakyytiApi.Models.RouteLogic;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Documents;
 using Microsoft.Azure.Documents.Client;
 using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 
 namespace KimppakyytiApi.Controllers
 {
@@ -28,7 +31,7 @@ namespace KimppakyytiApi.Controllers
             var endpointUri =
             _configuration["ConnectionStrings:CosmosDBConnection:EndpointUri"];
             var key =
-            _configuration["ConnectionStrings:CosmosDBConnection:PrimaryKey"];            
+            _configuration["ConnectionStrings:CosmosDBConnection:PrimaryKey"];
             _cosmosDBclient = new DocumentClient(new Uri(endpointUri), key);
             _cosmosDBclient.CreateDatabaseIfNotExistsAsync(new Database
             {
@@ -40,24 +43,72 @@ namespace KimppakyytiApi.Controllers
             new DocumentCollection { Id = _collectionName });
         }
 
+
+
         [HttpGet]
         public string Ping()
         {
             return "Nyt on tehty collection, vaikka sitä ei oltu tehty aiemmin!";
         }
+
+
         [HttpPost]
-        public async Task<ActionResult<string>> Post([FromBody] Ride value)
+        public async Task<ActionResult<string>> PostOfferRideAsync([FromBody] Ride value)
         {
-            Document document = await _cosmosDBclient.CreateDocumentAsync(
-          UriFactory.CreateDocumentCollectionUri(_dbName, _collectionName),
-          value);
+            //Functions for delayed response -- timeout try /catch
 
             // Get route from Google Directions Api
-            string response = await GoogleApiFunctions.GetRouteGoogle(value.StartAddress, value.TargetAddress);
-            // parse response to CosmoDB
+            var response = await GoogleApiFunctions.GetRouteGoogle(value.StartAddress, value.TargetAddress);
+            // parse response
+            RootObject obj = JsonConvert.DeserializeObject<RootObject>(response);
 
+            //return $"Start loc: Mysteerinollapolla:{obj.routes[0].legs[0].start_location.fakeNews} start lat: {obj.routes[0].legs[0].start_location.lat} start lng: {obj.routes[0].legs[0].start_location.lng} TargetLoc lat: {obj.routes[0].legs[0].end_location.lat} TargetLocation lng {obj.routes[0].legs[0].end_location.lng} ";
+
+            if (obj.status == "ZERO_RESULTS")
+            {
+                return "Reittiä ei löytynyt. Tarkista antamasi osoitteet, tai kokeile hakea kaupunginosalla.";
+            }
+            else if (obj.status == "OK")
+            {
+                //start and end point from Google to CosmosDB -object
+                value.StartLocation = new decimal[2];
+                value.StartLocation[0] = obj.routes[0].legs[0].start_location.lat;
+                value.StartLocation[1] = obj.routes[0].legs[0].start_location.lng;
+
+                value.TargetLocation = new decimal[2];
+                value.TargetLocation[0] = obj.routes[0].legs[0].end_location.lat;
+                value.TargetLocation[1] = obj.routes[0].legs[0].end_location.lng;
+                
+                //parse route points to Ride -object points in between - unless query is null
+                //if (obj.routes[0].legs[0].steps[0].end_location.lat != 0 && obj.routes[0].legs[0].steps[0].end_location.lng != 0)
+                //{
+                //    int i = 0;
+
+                //    foreach (var item in obj.routes[0].legs[0].steps)
+                //    {
+                //        value.RoutePoint[0] = item.end_location.lat;
+                //        value.RoutePoint[1] = item.end_location.lng;
+                //        i++;
+                //    }
+                //}
+
+                Document document = await _cosmosDBclient.CreateDocumentAsync(
+                UriFactory.CreateDocumentCollectionUri(_dbName, _collectionName),
+                value);
+
+                return Ok(document.ToString());
+            }
+            else
+            {
+                return "Nyt kävi jotain.";
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<string>> PostTestGoogleOnlyRoute([FromBody] Ride value)
+        {
+            var response = await GoogleApiFunctions.GetRouteGoogle(value.StartAddress, value.TargetAddress);
             return response;
-            //return Ok(document.Id);
         }
         [HttpGet]
         public ActionResult<List<Ride>> GetAllRides()
---
+++ kimppakyyti-api/KimppakyytiApi/Models/RouteLogic/RouteObject.cs
@@ -0,0 +1,165 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KimppakyytiApi.Models.RouteLogic
+{
+
+    public class RootObject
+    {
+        [JsonProperty("results")]
+        public List<GeocodedWaypoint> geocoded_waypoints { get; set; }
+        public List<Route> routes { get; set; }
+        public string status { get; set; }
+    }
+
+    public class GeocodedWaypoint
+    {
+        [JsonProperty("geocodedwaypoint")]
+        public string geocoder_status { get; set; }
+        public bool partial_match { get; set; }
+        public string place_id { get; set; }
+        public List<string> types { get; set; }
+    }
+
+    public class Northeast
+    {
+        [JsonProperty("northeast")]
+        public decimal fakeNews { get; set; }
+
+        public decimal lng { get; set; }
+        public decimal lat { get; set; }
+    }
+
+    public class Southwest
+    {
+        [JsonProperty("southwest")]
+        public decimal fakeNews { get; set; }
+        public decimal lng { get; set; }
+        public decimal lat { get; set; }
+    }
+
+    public class Bounds
+    {
+        [JsonProperty("bounds")]
+        public Northeast northeast { get; set; }
+        public Southwest southwest { get; set; }
+    }
+
+    public class Distance
+    {
+        [JsonProperty("distance")]
+        public string text { get; set; }
+        public int value { get; set; }
+    }
+
+    public class Duration
+    {
+        [JsonProperty("duration")]
+        public string text { get; set; }
+        public int value { get; set; }
+    }
+
+    public class EndLocation
+    {
+        [JsonProperty("endlocation")]
+        public decimal fakeNews { get; set; }
+        public decimal lng { get; set; }
+        public decimal lat { get; set; }
+    }
+
+    public class StartLocation
+    {
+        [JsonProperty("startlocation")]
+        public decimal fakeNews { get; set; }
+        public decimal lng { get; set; }
+        public decimal lat { get; set; }
+    }
+
+    public class Distance2
+    {
+        [JsonProperty("distance2")]
+        public string text { get; set; }
+        public int value { get; set; }
+    }
+
+    public class Duration2
+    {
+        [JsonProperty("duration2")]
+        public string text { get; set; }
+        public int value { get; set; }
+    }
+
+    public class EndLocation2
+    {
+        [JsonProperty("endlocation2")]
+        public decimal fakeNews { get; set; }
+        public decimal lng { get; set; }
+        public decimal lat { get; set; }
+    }
+
+    public class Polyline
+    {
+        [JsonProperty("polyline")]
+        public decimal fakeNews { get; set; }
+        public string points { get; set; }
+    }
+
+    public class StartLocation2
+    {
+        [JsonProperty("startlocation2")]
+        public decimal fakeNews { get; set; }
+        public decimal lng { get; set; }
+        public decimal lat { get; set; }
+    }
+
+    public class Step
+    {
+        [JsonProperty("step")]
+        public Distance2 distance { get; set; }
+        public Duration2 duration { get; set; }
+        public EndLocation2 end_location { get; set; }
+        public string html_instructions { get; set; }
+        public Polyline polyline { get; set; }
+        public StartLocation2 start_location { get; set; }
+        public string travel_mode { get; set; }
+        public string maneuver { get; set; }
+    }
+
+    public class Leg
+    {
+        [JsonProperty("leg")]
+        public Distance distance { get; set; }
+        public Duration duration { get; set; }
+        public string end_address { get; set; }
+        public EndLocation end_location { get; set; }
+        public string start_address { get; set; }
+        public StartLocation start_location { get; set; }
+        public List<Step> steps { get; set; }
+        public List<object> traffic_speed_entry { get; set; }
+        public List<object> via_waypoint { get; set; }
+    }
+
+    public class OverviewPolyline
+    {
+        [JsonProperty("overviewpolyline")]
+        public decimal fakeNews { get; set; }
+        public string points { get; set; }
+    }
+
+    public class Route
+    {
+        [JsonProperty("route")]
+        public Bounds bounds { get; set; }
+        public string copyrights { get; set; }
+        public List<Leg> legs { get; set; }
+        public OverviewPolyline overview_polyline { get; set; }
+        public string summary { get; set; }
+        public List<object> warnings { get; set; }
+        public List<object> waypoint_order { get; set; }
+    }
+}
+
+
