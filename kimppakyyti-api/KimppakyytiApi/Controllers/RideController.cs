701b65718c11d1d7d295e03fd964bdada9d9aad1
JukkaPeltola <jukka.peltola@mbnet.fi>
2018-08-24 11:07:49 +0300

    Googlelle haku ja palautus CosmosDB (PostOfferRideAsync)

--- kimppakyyti-api/KimppakyytiApi/Controllers/RideController.cs
+++ kimppakyyti-api/KimppakyytiApi/Controllers/RideController.cs
@@ -5,12 +5,14 @@ using System.Linq;
 using System.Security;
 using System.Threading.Tasks;
 using KimppakyytiApi.Models;
+using KimppakyytiApi.Models.RouteLogic;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Documents;
 using Microsoft.Azure.Documents.Client;
 using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 
 namespace KimppakyytiApi.Controllers
 {
@@ -133,6 +135,59 @@ namespace KimppakyytiApi.Controllers
             }
             return BadRequest();
         }
+
+        [HttpPost]
+        public async Task<ActionResult<string>> PostOfferRideAsync([FromBody] Ride value)
+        {
+            //Functions for delayed response -- timeout try /catch
+
+            // Get route from Google Directions Api
+            var response = await GoogleApiFunctions.GetRouteGoogle(value.StartAddress, value.TargetAddress);
+            // parse response
+            RootObject obj = JsonConvert.DeserializeObject<RootObject>(response);
+
+            //return $"Start loc: Mysteerinollapolla:{obj.routes[0].legs[0].start_location.fakeNews} start lat: {obj.routes[0].legs[0].start_location.lat} start lng: {obj.routes[0].legs[0].start_location.lng} TargetLoc lat: {obj.routes[0].legs[0].end_location.lat} TargetLocation lng {obj.routes[0].legs[0].end_location.lng} ";
+
+            if (obj.status == "ZERO_RESULTS")
+            {
+                return "Reittiä ei löytynyt. Tarkista antamasi osoitteet, tai kokeile hakea kaupunginosalla.";
+            }
+            else if (obj.status == "OK")
+            {
+                //start and end point from Google to CosmosDB -object
+                value.StartLocation = new decimal[2];
+                value.StartLocation[0] = obj.routes[0].legs[0].start_location.lat;
+                value.StartLocation[1] = obj.routes[0].legs[0].start_location.lng;
+
+                value.TargetLocation = new decimal[2];
+                value.TargetLocation[0] = obj.routes[0].legs[0].end_location.lat;
+                value.TargetLocation[1] = obj.routes[0].legs[0].end_location.lng;
+
+                //parse route points to Ride -object points in between - unless query is null
+                //if (obj.routes[0].legs[0].steps[0].end_location.lat != 0 && obj.routes[0].legs[0].steps[0].end_location.lng != 0)
+                //{
+                //    int i = 0;
+
+                //    foreach (var item in obj.routes[0].legs[0].steps)
+                //    {
+                //        value.RoutePoint[0] = item.end_location.lat;
+                //        value.RoutePoint[1] = item.end_location.lng;
+                //        i++;
+                //    }
+                //}
+
+                Document document = await _cosmosDBclient.CreateDocumentAsync(
+                UriFactory.CreateDocumentCollectionUri(_dbName, _collectionName),
+                value);
+
+                return Ok(document.ToString());
+            }
+            else
+            {
+                return "Nyt kävi jotain.";
+            }
+        }
+
         [HttpDelete]
         public async Task<ActionResult<string>> DeleteRide(string documentId)
         {
