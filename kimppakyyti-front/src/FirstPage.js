f76f5f7ba287e6b1eb89c989edcc13b2fbbf6764
jeremor <jere.morsky@gmail.com>
2018-08-27 10:57:03 +0300

    Etsi ja Offer laitettu täbeihin ja offerin postin pitäisi toimia

--- kimppakyyti-front/src/FirstPage.js
+++ kimppakyyti-front/src/FirstPage.js
@@ -1,17 +1,72 @@
 import React, { Component } from "react";
-import Time from "./components/Time";
-import Date from "./components/Date";
 import { Button } from "reactstrap";
 import AddNewProfile from "./components/AddProfile";
+<<<<<<< Updated upstream
 import { BrowserRouter as Router, Redirect } from "react-router-dom";
+=======
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+  Link
+} from "react-router-dom";
+import DatePicker from "react-datepicker";
+import moment from "moment";
+import './FirstPage.css';
+import "react-datepicker/dist/react-datepicker.css";
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+
+
+
+// import ApiCalendar from "react-google-calendar-api";
+>>>>>>> Stashed changes
+
+const posturl = "http://kimppakyytiapi.azurewebsites.net/api/ride/postofferrideasync"
+
+export function OfferNewRide(offer) {
+  fetch(posturl, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      startAddress: offer.startAddress,
+      targetAddress: offer.targetAddress,
+      startTime: offer.startTime,
+      endTime: offer.endTime,
+      mondayFrequency: offer.mondayFrequency,
+      tuesdayFrequency: offer.tuesdayFrequency,
+      wednesdayFrequency: offer.wednesdayFrequency,
+      thursdayFrequency: offer.thursdayFrequency,
+      fridayFrequency: offer.fridayFrequency,
+      saturdayFrequency: offer.saturdayFrequency,
+      sundayFrequency: offer.sundayFrequency
+    })
+  }).then(res => console.log(res));
+}
 
 class FirstPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      redirect: false
+      offer: {},
+      redirect: false,
+      startTime: moment(),
+      endTime: moment(),
+      
     };
+    this.startTimeChanged = this.startTimeChanged.bind(this);
+    this.endTimeChanged = this.endTimeChanged.bind(this);
   }
+  
+  
+  startTimeChanged(st){
+    this.setState({startTime: st});
+  }
+
+  endTimeChanged(et){
+    this.setState({endTime: et});
+  }
+
 
   movetoRideSearch = () => {
     this.setState({
@@ -20,12 +75,34 @@ class FirstPage extends Component {
     return;
   };
 
+
+
+  OfferRide = e => {
+    let informationTemp = {
+      startAddress: this.refs.startAddress.value,
+      targetAddress: this.refs.targetAddress.value, 
+      startTime: this.state.startTime,
+      endTime: this.state.endTime,
+      mondayFrequency: this.refs.monday.checked,
+      tuesdayFrequency: this.refs.tuesday.checked,
+      wednesdayFrequency: this.refs.wednesday.checked,
+      thursdayFrequency: this.refs.thursday.checked,
+      fridayFrequency: this.refs.friday.checked,
+      saturdayFrequency: this.refs.saturday.checked,
+      sundayFrequency: this.refs.sunday.checked
+    };
+    console.log(informationTemp);
+    OfferNewRide(informationTemp);
+    this.setState({offer: {}, redirect: true});
+  }
+
   render() {
     if (this.state.redirect === true) {
       return <Redirect to={`/ridesearchpage`} />;
     } else {
       return (
         <div>
+<<<<<<< Updated upstream
           <label>Mistä: </label>
           <input
             maxLength="50"
@@ -61,6 +138,154 @@ class FirstPage extends Component {
         </div>
       );
     }
+=======
+          <Tabs id="uncontrolled-tab-example">
+    <TabList>
+      <Tab>Tarjoan Kyytiä</Tab>
+      <Tab>Etsin Kyytiä</Tab>
+    </TabList>
+
+    <TabPanel>
+      <h2><label>Mistä: </label>
+        <input
+          maxLength="50"
+          onClick={this.xxx}
+          ref="startAddress"
+          type="text"
+          required
+          />{" "}
+        <br />
+        <label>Minne: </label>
+        <input
+          maxLength="50"
+          onClick={this.xxx}
+          ref="targetAddress"
+          type="text"
+          required
+          />
+        <br />
+        
+          <div class="center left">
+          <label>Valitse aikaväli miltä haet kyytiä</label>
+            <DatePicker
+          selected={this.state.startTime}
+          onChange={this.startTimeChanged}
+          showTimeSelect
+          timeFormat="HH:mm"
+          timeIntervals={15}
+          dateFormat="YYYY-MM-DD HH:mm"
+          timeCaption="time"
+          />
+          <DatePicker
+          selected={this.state.endTime}
+          onChange={this.endTimeChanged}
+          showTimeSelect
+          timeFormat="HH:mm"
+          timeIntervals={15}
+          dateFormat="YYYY-MM-DD HH:mm"
+          timeCaption="time"/>
+          <br/>
+          <label>Toistuvat päivät</label>
+          <br/>
+          
+          <label>
+            <input ref="monday" type="checkbox" />
+            Ma
+          </label>
+          <label>
+            <input ref="tuesday" type="checkbox"/>
+            Ti
+          </label>
+          <label>
+            <input ref="wednesday" type="checkbox"/>
+            Ke
+          </label>
+          <label>
+            <input ref="thursday" type="checkbox"/>
+            To
+          </label>
+          <label>
+            <input ref="friday" type="checkbox"/>
+            Pe
+          </label>
+          <label>
+            <input ref="saturday" type="checkbox"/>
+            La
+          </label>
+          <label>
+            <input ref="sunday" type="checkbox"/>
+            Su
+          </label>
+          <br/>
+          <Button type="submit" onClick={this.OfferRide}>Jeesusnappi</Button>
+        </div>
+        
+        
+        <Button outline color="secondary" onClick={this.OfferRide} type="submit">
+          Tarjoa kyytiä
+        </Button>{" "}
+        </h2>
+    </TabPanel>
+    <TabPanel>
+      <h2><label>Mistä: </label>
+        <input
+          maxLength="50"
+          onClick={this.xxx}
+          ref="startAddress"
+          type="text"
+          required
+          />{" "}
+        <br />
+        <label>Minne: </label>
+        <input
+          maxLength="50"
+          onClick={this.xxx}
+          ref="targetAddress"
+          type="text"
+          required
+          />
+        <br />
+        
+          <div class="center left">
+          <label>Valitse aikaväli miltä haet kyytiä</label>
+            <DatePicker
+          selected={this.state.startTime}
+          onChange={this.startTimeChanged}
+          showTimeSelect
+          timeFormat="HH:mm"
+          timeIntervals={15}
+          dateFormat="YYYY-MM-DD HH:mm"
+          timeCaption="time"
+          />
+          <DatePicker
+          selected={this.state.endTime}
+          onChange={this.endTimeChanged}
+          showTimeSelect
+          timeFormat="HH:mm"
+          timeIntervals={15}
+          dateFormat="YYYY-MM-DD HH:mm"
+          timeCaption="time"/>
+          
+          <label>
+            <input ref="frequent" type="checkbox" />
+            Toistuva
+          </label>
+          <br/>
+          <Button type="submit" onClick={this.dateTimeValue}>Jeesusnappi</Button>
+        </div>
+        
+        <Button outline color="secondary" onClick={this.movetoRideSearch} type="button">
+          Etsi kyytiä
+        </Button>{" "}</h2>
+    </TabPanel>
+  </Tabs>
+        
+        <AllProfiles />
+        <AddNewProfile />
+      </div>
+    );
+  }
+>>>>>>> Stashed changes
   }
 }
 
--- kimppakyyti-front/src/RideSearchPage.js
+++ kimppakyyti-front/src/RideSearchPage.js
@@ -6,18 +6,36 @@ import {
     Redirect,
     Link
   } from "react-router-dom";
-  import FirstPage from "./FirstPage";
+import FirstPage from "./FirstPage";
 import RideBox from "./components/RideBox";
 import AddNewRide from "./components/AddRide";
 
-  class RideSearchPage extends Component {
-      render() {
+class RideSearchPage extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            redirect: false
+        }
+     };
+
+    goBack = () => {
+        this.setState({redirect: true});
+    }
+    render() {
+        if (this.state.redirect === true){
+            return (
+              <Redirect to={`/firstpage`}/>
+            );
+          }else {
+      
           return (
               <div>
                   <AddNewRide/>
                   <RideBox/>
+                  <button onClick={this.goBack}>Back</button>
               </div>
           )
+        }
       }
   }
 
