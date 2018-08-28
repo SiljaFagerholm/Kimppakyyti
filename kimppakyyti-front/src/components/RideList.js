a0322e3c94ea12e799dfd0b048b1c2d8aad3cae1
melaaman <erika.laamanen@gmail.com>
2018-08-28 11:40:40 +0300

    post toimii eikä ridelistin maps-funktio näytä erroria

--- kimppakyyti-front/src/components/RideList.js
+++ kimppakyyti-front/src/components/RideList.js
@@ -3,13 +3,8 @@ import Ride from "./Ride";
 
 class RideList extends Component {
   render() {
-<<<<<<< HEAD
     var everything = this.props.rides.map(function(ride, i) {
       return <Ride singleride={ride} key={i} />;
-=======
-    var everything = this.props.rides.map(function(ride) {
-      return <Ride singleride={ride} key={ride.id} />;
->>>>>>> master
     });
     return (
       <div>
