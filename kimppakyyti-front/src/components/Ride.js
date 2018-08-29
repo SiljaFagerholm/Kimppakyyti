b9b8768f8646189ab0259eaf82dafb9db90b8200
melaaman <erika.laamanen@gmail.com>
2018-08-29 16:33:27 +0300

    toimiva versio taas jotta ei katoa TAAS

--- kimppakyyti-front/src/components/Ride.js
+++ kimppakyyti-front/src/components/Ride.js
@@ -1,23 +1,24 @@
-import React, {Component} from 'react';
-
+import React, { Component } from "react";
 
 class Ride extends Component {
-    render(){
-        return (
-                <div>
-                    <p>Nickname: {this.props.singleride.nickname}</p>
-                    <p>Mistä: {this.props.singleride.startAddress}</p>
-                    <p>Mihin: {this.props.singleride.targetAddress}</p>
-                    <p>Aikaväli: {this.props.singleride.startTime} - {this.props.singleride.endTime}</p>
-                    <p>Milloin: {this.props.singleride.when}</p>
-                    <p>Hinta: {this.props.singleride.price}</p>
-                    <p>
-                    <button>Liity kyytiin</button>
-
-                    </p>
-                </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <p>Nickname: {this.props.singleride.nickname}</p>
+        <p>Mistä: {this.props.singleride.startAddress}</p>
+        <p>Mihin: {this.props.singleride.targetAddress}</p>
+        <p>
+          Aikaväli: {this.props.singleride.startTime} -{" "}
+          {this.props.singleride.endTime}
+        </p>
+        <p>Milloin: {this.props.singleride.when}</p>
+        <p>Hinta: {this.props.singleride.price}</p>
+        <p>
+          <button>Liity kyytiin</button>
+        </p>
+      </div>
+    );
+  }
 }
 
 export default Ride;
